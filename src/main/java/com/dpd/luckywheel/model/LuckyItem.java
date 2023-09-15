f42b43305fd02e78596cee01f2d8e7a0f4cef4bb
Dannv-mk1 <43530446+nguyenvandan0003@users.noreply.github.com>
2023-09-15 16:57:17 +0700

    Initial commit

---
+++ src/main/java/com/dpd/luckywheel/model/LuckyItem.java
@@ -0,0 +1,11 @@
+package com.dpd.luckywheel.model;
+
+/**
+ * Created by kiennguyen on 11/5/16.
+ */
+
+public class LuckyItem {
+    public String text;
+    public int icon;
+    public int color;
+}
